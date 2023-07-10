e817338ae0aa05d240966af6d0f7c095974a4fbc
Rasvom <rasvomb@gmail.com>
2023-07-10 15:35:00 +0300

    add cart store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import application from "../features/applicationSlice";
+import cart from "../features/cartSlice";
 
 export const store = configureStore({
   reducer: {
     application,
+    cart,
   },
 });

267fed0246af6bb278a55ae8d35764cb1aa3df9d
Rasvom <rasvomb@gmail.com>
2023-07-10 15:33:25 +0300

    add cart

---
+++ src/features/cartSlice.ts
@@ -0,0 +1,51 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  cart: [],
+  error: "",
+  status: false,
+};
+
+export const getUserCart = createAsyncThunk(
+  "cart/fetchUserCart",
+  async (_, thunkAPI) => {
+    try {
+      const res = await fetch("http://localhost:4000/user-cart", {
+        method: "GET",
+        headers: {
+          "Content-type": "application/json",
+          Authorization: `Bearer ${thunkAPI.getState().application.token}`,
+        },
+      });
+      const json = await res.json();
+      if (json.error) {
+        return thunkAPI.rejectWithValue(json.error);
+      }
+      return json;
+    } catch (error) {
+      thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(getUserCart.pending, (state) => {
+        state.status = true;
+      })
+      .addCase(getUserCart.rejected, (state, action) => {
+        state.status = false;
+        state.error = action.payload;
+      })
+      .addCase(getUserCart.fulfilled, (state, action) => {
+        state.status = false;
+        state.cart = action.payload;
+      });
+  },
+});
+
+export default cartSlice.reducer;

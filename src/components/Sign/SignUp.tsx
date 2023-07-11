4364cef78de5d0a0b49855197aea70c9b76faae8
shishanee <mansur72095@gmail.com>
2023-07-11 16:14:47 +0300

    sign ready

--- src/components/Sign/SignUp.tsx
+++ src/components/Sign/SignUp.tsx
@@ -1,15 +1,17 @@
-import { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { authSignUp } from "../../features/applicationSlice";
 import { Link, useNavigate } from "react-router-dom";
 import photo from "../../../public/Foto.png";
 import styles from "./SignIn.module.css";
 
-
-
 const SignUp = () => {
+  const [isSign, setIsSign] = useState(false);
+
   const [login, setLogin] = useState("");
   const [password, setPassword] = useState("");
+
+  const error = useSelector((state) => state.application.error);
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -24,35 +26,44 @@ const SignUp = () => {
 
   const handleRegister = () => {
     dispatch(authSignUp({ login, password }));
-    setTimeout(() => {
-      navigate("/login");
-    }, 500);
+    if (error) {
+      setIsSign(true);
+    }
   };
+
+  useEffect(() => {
+    if (isSign && !error) {
+      navigate("/login");
+    }
+  }, [error, navigate, isSign]);
 
   return (
     <div className={styles.mainBlockIn}>
-    <div className={styles.blockOne}>
-      <div className={styles.form}>
-        <p>Создайте аккаунт</p>
-        <input
-          onChange={changeLogin}
-          value={login}
-          type="text"
-          placeholder="Введите логин"
-        />
-        <input
-          onChange={changePassword}
-          value={password}
-          type="password"
-          placeholder="Введите пароль"
-        />
-        <button onClick={handleRegister}>Зарегистрироваться</button>
+      <div className={styles.blockOne}>
+        <div className={styles.form}>
+          <p>Создайте аккаунт</p>
+          <input
+            onChange={changeLogin}
+            value={login}
+            type="text"
+            placeholder="Введите логин"
+          />
+          <input
+            onChange={changePassword}
+            value={password}
+            type="password"
+            placeholder="Введите пароль"
+          />
+          <button onClick={handleRegister}>Зарегистрироваться</button>
+          <br />
+
+          {error}
+        </div>
+      </div>
+      <div className={styles.imageBlock}>
+        <img src={photo} alt="" />
       </div>
     </div>
-    <div className={styles.imageBlock}>
-      <img src={photo} alt="" />
-    </div>
-  </div>
   );
 };
 
